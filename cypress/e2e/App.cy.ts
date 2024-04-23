5b7b7816e8e289424a56312e5cf6bf230a1093a5
afreeca mendes <adilsonmendesramos@gmail.com>
2024-04-23 10:38:48 +0100

    remove undone bits from the readme

---
+++ cypress/e2e/App.cy.ts
@@ -0,0 +1,13 @@
+describe('User should be able to play game one', () => {
+  it('User should visualise their account details', () => {
+    cy.visit('http://localhost:3000');
+  });
+
+  it('User should visualise a message for not accounts', () => {});
+
+  it('User should visualise the selected account transactions', () => {});
+
+  it('User should visualise a message for no transactions', () => {});
+
+  it('User should calculate and transfer round up amount', () => {});
+});
